483dcd9daea3b052a1e09f59206bf3d126993d83
Hirusha_R <it22283344@my.sliit.lk>
2025-01-11 10:46:06 +0530

    sign in page done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+      <div className='text-center text-blue-600 text-4xl animate-bounce font-mono'>
+      
+
+
+      </div>
+      <div>
+
+      </div>
+      about
+
+    </div>
+  )
+}
